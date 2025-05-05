a85781ba109dcc260110e38104e5d28e573eee69
Franciscoxd1123 <francisco.cea@usach.cl>
2025-05-04 21:42:13 -0400

    Arreglos de 1 error de seguridad

--- prestabanco-backend/src/main/java/com/prestabanco/PrestabancoAppApplication.java
+++ prestabanco-backend/src/main/java/com/prestabanco/PrestabancoAppApplication.java
@@ -3,7 +3,6 @@ package com.prestabanco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Holi
 
 @SpringBootApplication
 public class PrestabancoAppApplication {
